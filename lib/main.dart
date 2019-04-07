072d6949f6d0e310c81af7e1b0740172832bc68a
Pongsiri Chuaychoonoo <savepong.com@gmail.com>
2019-04-07 22:31:22 +0700

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   static const double RADIUS_CORNER = 12;
   static const int NONE = 0;
   static const int VALUE_X = 1;
   static const int VALUE_O = 2;
 
+  // Game Theme
+  Color colorBorder = Colors.green[600];
+  Color colorBackground = Colors.green[100];
+  Color colorBackgroundChannelNone = Colors.green[200];
+  Color colorBackgroundChannelValueX = Colors.green[400];
+  Color colorBackgroundChannelValueO = Colors.green[400];
+  Color colorChannelIcon = Colors.green[800];
+  Color colorTextCurrentTurn = Colors.green[900];
+
+  // Game Status
   List<List<int>> channelStatus = [
     [VALUE_X, NONE, VALUE_X],
     [NONE, VALUE_X, NONE],
     [VALUE_X, NONE, VALUE_X],
   ];
+
+  int currentTurn = VALUE_X;
 
   @override
   Widget build(BuildContext context) {
@@ -43,92 +56,273 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title)
       ),
       body: Container(constraints: BoxConstraints.expand(),
-        color: Colors.green[100],
+        color: colorBackground,
         child: Center(
-          child: Container(color: Colors.green[400],
-            padding: EdgeInsets.all(8),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(mainAxisSize: MainAxisSize.min,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text("Turn of player",
+                style: TextStyle(
+                  fontSize: 36,
+                  color: colorTextCurrentTurn,
+                  fontWeight: FontWeight.bold
+                )
+              ),
+              Icon(getIconFromStatus(currentTurn), size: 60, color: colorChannelIcon),
+              Container(
+                margin: EdgeInsets.only(top: 12),
+                decoration: BoxDecoration(
+                  color: colorBorder,
+                  borderRadius: BorderRadius.all(Radius.circular(8))
+                ),
+                padding: EdgeInsets.all(12),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    buildChannel(RADIUS_CORNER, 0, 0, 0, channelStatus[0][0]),
-                    buildChannel(0, 0, 0, 0, channelStatus[0][1]),
-                    buildChannel(0, RADIUS_CORNER, 0, 0, channelStatus[0][2]),
-                  ],
-                ),
-                Row(mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    buildChannel(0, 0, 0, 0, channelStatus[1][0]),
-                    buildChannel(0, 0, 0, 0, channelStatus[1][1]),
-                    buildChannel(0, 0, 0, 0, channelStatus[1][2]),
-                  ],
-                ),
-                Row(mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    buildChannel(0, 0, RADIUS_CORNER, 0, channelStatus[2][0]),
-                    buildChannel(0, 0, 0, 0, channelStatus[2][1]),
-                    buildChannel(0, 0, 0, RADIUS_CORNER, channelStatus[2][2]),
-                  ],
-                ),
-              ],
-            )
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: buildRowChannel(0)
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: buildRowChannel(1)
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: buildRowChannel(2)
+                    ),
+                  ]
+                )
+              )
+            ],
           )
         )
       )
     );
   }
-}
 
-Container buildChannel(
-    double tlRadius,
-    double trRadius,
-    double blRadius,
-    double brRadius,
-    int status
-  ) => 
-  Container(
-    margin: EdgeInsets.all(2), 
-    width: 100, 
-    height: 100, 
-    decoration: BoxDecoration(
-      color: Colors.green[100],
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(tlRadius),
-        topRight: Radius.circular(trRadius),
-        bottomLeft: Radius.circular(blRadius),
-        bottomRight: Radius.circular(brRadius)
-      )
-    ),
-    child: Icon(getIconFromStatus(status), size: 60, color: Colors.green[800])
+
+  List<Widget> buildRowChannel(int row) {
+    List<Widget> listWidget = List();
+    for (int col = 0; col < 3; col++) {
+      double tlRadius = row == 0 && col == 0 ? RADIUS_CORNER : 0;
+      double trRadius = row == 0 && col == 2 ? RADIUS_CORNER : 0;
+      double blRadius = row == 2 && col == 0 ? RADIUS_CORNER : 0;
+      double brRadius = row == 2 && col == 2 ? RADIUS_CORNER : 0;
+      Widget widget = buildChannel(
+        row,
+        col,
+        tlRadius,
+        trRadius,
+        blRadius,
+        brRadius,
+        channelStatus[row][col]);
+      listWidget.add(widget);
+    }
+    return listWidget;
+  }
+
+Widget buildChannel(
+  int row,
+  int col,
+  double tlRadius,
+  double trRadius,
+  double blRadius,
+  double brRadius,
+  int status) => 
+  GestureDetector(onTap: () => onChannelPressed(row, col),
+    child: Container(
+      margin: EdgeInsets.all(2), 
+      width: 100, 
+      height: 100, 
+      decoration: BoxDecoration(
+        color: getBackgroundChannelFromStatus(status),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(tlRadius),
+          topRight: Radius.circular(trRadius),
+          bottomLeft: Radius.circular(blRadius),
+          bottomRight: Radius.circular(brRadius))),
+      child: Icon(getIconFromStatus(status), size: 60, color: colorChannelIcon))
   );
-
+  
   IconData getIconFromStatus(int status) {
-    if (status == 1) {
+    if (status == VALUE_X) {
       return Icons.close;
-    } else if (status == 2) {
+    } else if (status == VALUE_O) {
       return Icons.radio_button_unchecked;
     }
     return null;
   }
 
-  Color colorBorder = Colors.green[600];
-  Color colorBackground = Colors.green[100];
-  Color colorBackgroundChannelNone = Colors.green[200];
-  Color colorBackgroundChannelValueX = Colors.green[400];
-  Color colorBackgroundChannelValueO = Colors.green[400];
-  Color colorChannelIcon = Colors.green[800];
-
-
   Color getBackgroundChannelFromStatus(int status) {
-    if (status == 1) {
+    if (status == VALUE_X) {
       return colorBackgroundChannelValueX;
-    } else if (status == 2) {
+    } else if (status == VALUE_O) {
       return colorBackgroundChannelValueO;
     }
     return colorBackgroundChannelNone;
   }
+
+  onChannelPressed(int row, int col) {
+    if (channelStatus[row][col] == NONE) {
+      setState(() {
+        channelStatus[row][col] = currentTurn;
+
+        if (isGameEndedByWin()) {
+          showEndGameDialog(currentTurn);
+        } else {
+          if(isGameEndedByDraw()){
+            showEndGameByDrawDialog();
+          } else {
+            switchPlayer();
+          }
+        }
+      });
+    }
+  }
+
+  void switchPlayer() {
+    setState(() {
+      if (currentTurn == VALUE_X) {
+        currentTurn = VALUE_O;
+      } else if (currentTurn == VALUE_O) {
+        currentTurn = VALUE_X;
+      }
+    });
+  }
+
+  bool isGameEndedByDraw() {
+    for (int row = 0; row < 3; row++) {
+      for (int col = 0; col < 3; col++) {
+        if(channelStatus[row][col] == NONE){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  bool isGameEndedByWin() {
+    // check vertical
+    for (int col = 0; col < 3; col++) {
+      if (
+        channelStatus[0][col] != NONE 
+        && channelStatus[0][col] == channelStatus[1][col] 
+        && channelStatus[1][col] == channelStatus[2][col]
+      ) {
+        return true;
+      }
+    }
+
+    // check horizontal
+    for (int row = 0; row < 3; row++) {
+      if (
+        channelStatus[row][0] != NONE 
+        && channelStatus[row][0] == channelStatus[row][1] 
+        && channelStatus[row][1] == channelStatus[row][2]
+      ) {
+        return true;
+      }
+    }
+
+    // check cross left to right.
+    if (channelStatus[0][0] != NONE
+      && channelStatus[0][0] == channelStatus[1][1]
+      && channelStatus[1][1] == channelStatus[2][2]
+    ) {
+      return true;
+    }
+
+    // check cross right to left.
+    if (channelStatus[0][2] != NONE
+      && channelStatus[0][2] == channelStatus[1][1]
+      && channelStatus[1][1] == channelStatus[2][0]
+    ){
+      return true;
+    }
+
+    return false;
+  }
+
+  void showEndGameDialog(int winner) {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          content: Column(mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text("The winner is", style: TextStyle(
+                fontSize: 32,
+                color: colorTextCurrentTurn,
+                fontWeight: FontWeight.bold)),
+              Icon(getIconFromStatus(currentTurn),
+                size: 60,
+                color: colorChannelIcon),
+              RaisedButton(padding: EdgeInsets.symmetric(horizontal: 18, vertical: 6),
+                color: Colors.yellow[800],
+                child: Text("Play again",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold)),
+                onPressed: () {
+                  playAgain();
+                  Navigator.of(context).pop();
+                },
+              )
+            ]
+          )
+        );
+      },
+    );
+  }
+
+  void showEndGameByDrawDialog() {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+            content: Column(mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text("Draw", style: TextStyle(
+                      fontSize: 32,
+                      color: colorTextCurrentTurn,
+                      fontWeight: FontWeight.bold)),
+                  RaisedButton(padding: EdgeInsets.symmetric(horizontal: 18, vertical: 6),
+                    color: Colors.yellow[800],
+                    child: Text("Play again",
+                        style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold)),
+                    onPressed: () {
+                      playAgain();
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ])
+        );
+      },
+    );
+  }
+
+  playAgain() {
+    setState(() {
+      currentTurn = VALUE_X;
+      channelStatus = [
+        [NONE, NONE, NONE],
+        [NONE, NONE, NONE],
+        [NONE, NONE, NONE],
+      ];
+    });
+  }
+
+}
